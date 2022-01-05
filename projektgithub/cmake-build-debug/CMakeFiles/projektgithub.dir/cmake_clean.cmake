c1039ee556bbe918b1dda459885507cbf3489f25
MarcinFrejnagel1 <97180223+MarcinFrejnagel1@users.noreply.github.com>
2022-01-05 16:43:42 +0100

    Add files via upload

---
+++ projektgithub/cmake-build-debug/CMakeFiles/projektgithub.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projektgithub.dir/main.cpp.obj"
+  "libprojektgithub.dll.a"
+  "projektgithub.exe"
+  "projektgithub.exe.manifest"
+  "projektgithub.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projektgithub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
